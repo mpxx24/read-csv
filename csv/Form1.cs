e904dc5204dbe7bf368b673e24e0c3b7aaae238a
mpxx24 <mpxx24@gmail.com>
2014-08-05 20:37:15 +0200

    2done - bd

--- csv/Form1.cs
+++ csv/Form1.cs
@@ -2,23 +2,27 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace csv
 {
     public partial class Form1 : Form
     {
-        //private const string Path = @"C:/mpx/mojcsv.csv";
         public Form1()
         {
             InitializeComponent();
         }
-        //ogranicz kol do 156
+        //ogranicz kol do 156?
+
+        //sposób #1 - line by line
         private void button1_Click(object sender, EventArgs e)
         {
             openFileDialog1.ShowDialog();
@@ -42,12 +46,76 @@ namespace csv
             dataGridView1.DataSource = tabela;
         }
 
+        //sposób #2 - jeden string
         private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            var plik = openFileDialog1.FileName;
+            var tabela = new DataTable();
+            var znak = ';';
+
+            var allData = File.ReadAllText(plik);
+            var linieTekstu = Regex.Split(allData, "\r\n").ToList();
+            var nazwyKolumn = linieTekstu.First().Split(znak);
+            //var linie = allData.Split().SkipWhile(x => x.Equals("\r\n"));// && x != "\r\n");
+
+            foreach (var s in nazwyKolumn)
+            {
+                tabela.Columns.Add(s);
+            }
+
+            linieTekstu.RemoveAt(1);
+            foreach (var s in linieTekstu)
+            {
+                tabela.Rows.Add(s.Split(znak));
+            }
+
+
+            dataGridView1.DataSource = tabela;
+
+
+        }
+
+        //sposób 3
+        private void button3_Click(object sender, EventArgs e)
         {
 
         }
 
+        //'wyczyść' DataGridView
+        private void button4_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+        }
 
+        private void button5_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            var plik = openFileDialog1.FileName;
 
+            var allData = File.ReadAllText(plik);
+
+            using (var polacznie = new SqlConnection("Data Source=localhost;Initial Catalog=OptimaDb;Integrated Security=True;"))
+            {
+                polacznie.Open();
+                try
+                {
+                    using (var polecenie = new SqlCommand("create table plikCSV (zawartosc TEXT)"))
+                    {
+                        polecenie.ExecuteNonQuery();
+                    }
+                    using (var polecenie = new SqlCommand("insert into plikcsv values (@zawartosc)"))
+                    {
+                        polecenie.Parameters.Add(new SqlParameter("zawartosc", allData));
+                        polecenie.ExecuteNonQuery();
+                    }
+                    
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show(e.ToString());
+                }
+            }
+        }
     }
 }
